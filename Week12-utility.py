b8fa59029ca9f2f363edee02b9be10c3911c441e
Sean Smith <48806520+Me7is@users.noreply.github.com>
2019-11-19 12:22:10 -0700

    Finished the code

--- Week12-utility.py
+++ Week12-utility.py
@@ -11,19 +11,55 @@ def LoadFile (file_name):
     return line_list
 
 def UpdateString (string_1, string_2, index):
-    print()
+    string_1_list = list(string_1)
+    string_1_list[index] = string_2
+    PrintOutput("".join(string_1_list))
 
 def FindWordCount (list, string):
-    return
+    count = 0
+    for i in range(len(list)):
+        if string == list[i]:
+            count += 1
+    return count
 
 def ScoreFinder (list_1, list_2, string):
-    return
+    exist = False
+    for i in range(len(list_1)):
+        if list_1[i].lower() == string.lower():
+            print("OUTPUT", list_1[i], "got a score of", list_2[i])
+            exist = True
+            break
+    if not exist:
+        print("OUTPUT player not found")
 
 def Union (list_1, list_2):
-    return
+    exist = False
+    for i in range(len(list_1)):
+        for x in range(len(list_2)):
+            if list_1[i] == list_2[x]:
+                exist = True
+        if not exist:
+            list_2.append(list_1[i])
+        exist = False
+    return list_2
 
 def Intersection (list_1, list_2):
-    return
+    list_3 = []
+    for i in range(len(list_1)):
+        for x in range(len(list_2)):
+            if list_1[i] == list_2[x]:
+                list_3.append(list_1[i])
+                break
+    return list_3
 
 def NotIn (list_1, list_2):
-    return
+    exist = False
+    list_3 = []
+    for i in range(len(list_1)):
+        for x in range(len(list_2)):
+            if list_1[i] == list_2[x]:
+                exist = True
+        if exist == False:
+            list_3.append(list_1[i])
+        exist = False
+    return list_3
